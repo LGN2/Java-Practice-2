f1b26e9e1f8bc12ae02382be405a6c1466953bc8
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 16:59:43 +0400

    final v

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,20 +1,20 @@
 public class FrequencyCounter {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Integer number = 122333;
         Integer org = number;
         Integer targetDigit = 3;
         Integer count = 0;
         Integer digit;
 
-        while (number != 0){
-            digit = number % 2;
-            if (digit.equals(targetDigit)){
+        while (number != 0) {
+            digit = number % 10;
+            if (digit.equals(targetDigit)) {
                 count++;
             }
             number = number / 10;
         }
-        IO.println("Number:" +org);
-        IO.println("Target Digit:" +targetDigit);
-        IO.println("Frequency:" +count);
+        IO.println("Number:" + org);
+        IO.println("Target Digit:" + targetDigit);
+        IO.println("Frequency:" + count);
     }
 }
