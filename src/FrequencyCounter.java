946a85b06b69b79d4543e1bfde1a2668103847ed
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 16:33:44 +0400

    working

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,5 +1,9 @@
 public class FrequencyCounter {
     public static void main(String[] args){
+        Integer number;
+        Integer targetDigit;
+        Integer count;
+        Integer digit;
         
     }
 }
