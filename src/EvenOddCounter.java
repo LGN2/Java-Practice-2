2f229fe91a59071b6dff9069e3cd005f87b4560c
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 14:29:30 +0400

    final v

--- src/EvenOddCounter.java
+++ src/EvenOddCounter.java
@@ -1,10 +1,18 @@
 public class EvenOddCounter {
     public static void main(String[] args){
-        Integer start;
-        Integer end;
+        Integer start = 1;
+        Integer end = 10;
         Integer evenCount = 0;
         Integer oddCount = 0;
 
-        
+        for (int i = start; i <= end; i++){
+            if (i % 2 ==0){
+                evenCount++;
+            }else {
+                oddCount++;
+            }
+        }
+        IO.println("Even Numbers Count:"+evenCount);
+        IO.println("Odd Numbers Count:"+oddCount);
     }
 }
