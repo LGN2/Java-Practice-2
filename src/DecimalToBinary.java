bb6327ad6b26133b2193aa2a20c7dac703fd9ca3
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 16:24:53 +0400

    final v

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -1,16 +1,16 @@
 public class DecimalToBinary {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Integer number = 25;
         Integer org = number;
         String binary = "";
         Integer remainder;
 
-        while (number > 0){
+        while (number > 0) {
             remainder = number % 2;
             binary = remainder + binary;
             number = number / 2;
         }
-        IO.println("Decimal Number:"+org);
-        IO.println("Binary Number:"+binary);
+        IO.println("Decimal Number:" + org);
+        IO.println("Binary Number:" + binary);
     }
 }
