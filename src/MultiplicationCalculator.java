aacf0098bdedf05ce9f7f6304cd46b275c79881c
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 15:24:48 +0400

    final v

--- src/MultiplicationCalculator.java
+++ src/MultiplicationCalculator.java
@@ -1,13 +1,13 @@
 public class MultiplicationCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Integer start = 1;
         Integer end = 5;
         Integer result = 1;
 
-        for (int i = start; i <= end; i++){
+        for (int i = start; i <= end; i++) {
             result *= i;
         }
-        IO.println("multiplication Result:" +result);
+        IO.println("multiplication Result:" + result);
 
     }
 }
