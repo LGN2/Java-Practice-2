842c55ddbaa17077354b00ff434cf67ad6a05d01
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 17:41:48 +0400

    working on it

--- src/ArmstrongNumberChecker.java
+++ src/ArmstrongNumberChecker.java
@@ -1,5 +1,11 @@
 public class ArmstrongNumberChecker {
     public static void main(String[] args){
+        Integer number = 153;
+        Integer org = number;
+        Integer digit;
+        Integer sum = 0;
+        Integer count = 0;
+
         
     }
 }
