fcbd5daa3121c375f8a6f7144e1b830ae23b55bc
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 15:24:26 +0400

    final v

---
+++ src/MultiplicationCalculator.java
@@ -0,0 +1,13 @@
+public class MultiplicationCalculator {
+    public static void main(String[] args){
+        Integer start = 1;
+        Integer end = 5;
+        Integer result = 1;
+
+        for (int i = start; i <= end; i++){
+            result *= i;
+        }
+        IO.println("multiplication Result:" +result);
+
+    }
+}
