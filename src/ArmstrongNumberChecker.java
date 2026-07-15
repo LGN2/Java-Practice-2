f645c4984958f5ec4c65c7ec712e48ea8773f55b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 17:53:55 +0400

    stuck

--- src/ArmstrongNumberChecker.java
+++ src/ArmstrongNumberChecker.java
@@ -6,6 +6,25 @@ public class ArmstrongNumberChecker {
         Integer sum = 0;
         Integer count = 0;
 
-        
+        while (number != 0){
+            count++;
+            number = number / 10;
+        }
+        while (number != 0){
+            digit = number % 10;
+            int power = 1;
+            for (int i = 1; i <= count; i++){
+                power *= digit;
+            }
+            sum += power;
+            number = number / 10;
+        }
+        IO.println("Number:"+org);
+
+        if (sum.equals(org)){
+            IO.println("The Number is an Armstrong Number.");
+        }else {
+            IO.println("The Number is Not an Armstrong Number.");
+        }
     }
 }
