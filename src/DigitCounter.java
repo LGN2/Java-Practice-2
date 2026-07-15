8037e32dae3b276365d19632c5f26faf3e387bcd
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 11:45:40 +0400

    final v

--- src/DigitCounter.java
+++ src/DigitCounter.java
@@ -1,14 +1,14 @@
 public class DigitCounter {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Integer number = 123456;
         Integer org = number;
         Integer count = 0;
 
-        while (number != 0){
+        while (number != 0) {
             number = number / 10;
             count++;
         }
-        IO.println("Number:"+org);
-        IO.println("Number of digits:"+count);
+        IO.println("Number:" + org);
+        IO.println("Number of digits:" + count);
     }
 }
