9f14b6de3f2fefe62fa6165529b38a27943e9cc7
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 16:41:25 +0400

    nearly finished

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,9 +1,16 @@
 public class FrequencyCounter {
     public static void main(String[] args){
-        Integer number;
-        Integer targetDigit;
+        Integer number = 122333;
+        Integer targetDigit = 3;
         Integer count;
         Integer digit;
-        
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
     }
 }
