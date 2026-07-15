efad9088fdb7d475ef05c3f058631d89e9a9914b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-07-15 16:53:13 +0400

    stuck

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,16 +1,20 @@
 public class FrequencyCounter {
     public static void main(String[] args){
         Integer number = 122333;
+        Integer org = number;
         Integer targetDigit = 3;
-        Integer count;
+        Integer count = 0;
         Integer digit;
 
-        for (int i = number; i <= number; i++) {
-            if (i % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
+        while (number != 0){
+            digit = number % 2;
+            if (digit.equals(targetDigit)){
+                count++;
             }
+            number = number / 10;
         }
+        IO.println("Number:" +org);
+        IO.println("Target Digit:" +targetDigit);
+        IO.println("Frequency:" +count);
     }
 }
